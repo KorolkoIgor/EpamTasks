aa6193f43b8cebd0def59c64fe5a0276a8adab7b
KorolkoIgor <korolko_igor@mail.ru>
2016-05-04 12:51:43 +0300

    Signed-off-by: KorolkoIgor <korolko_igor@mail.ru>

---
+++ PhoneStation/PhoneStation/ITerminal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneStation
+{
+    public interface ITerminal
+    {
+       PhoneNumber Number { get; }
+       Contract Contract { get; }
+       Port Link { get; }
+
+      
+
+        event EventHandler<Request> CallFrom;
+        event EventHandler<RegisterTerminal> RegisterTerminal;
+        event EventHandler AcceptCall;
+        event EventHandler EndCall;
+
+        void Call(Terminal target);
+        void Answer();
+        void Drop();
+        void OnPort();
+        bool IsPortOnStation();
+        void ClearEvents();
+       
+    }
+}
---
+++ PhoneStation/PhoneStation/Terminal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneStation
+{
+    public class Terminal:ITerminal
+    {
+        public PhoneNumber Number { get; set; }
+        public Contract Contract { get; set; }
+        public Port Link { get; set; }
+
+
+
+
+        public event EventHandler<Request> CallFrom;
+
+        public event EventHandler<RegisterTerminal> RegisterTerminal;
+
+        public event EventHandler AcceptCall;
+
+        public event EventHandler EndCall;
+
+        public void Call(Terminal target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Answer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPortOnStation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
