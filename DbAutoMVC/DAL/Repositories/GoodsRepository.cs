4f4fa63dd836763d8025c00cfb7e875429d204e4
KorolkoIgor <korolko_igor@mail.ru>
2016-06-10 23:49:22 +0300

    Signed-off-by: KorolkoIgor <korolko_igor@mail.ru>

---
+++ DbAutoMVC/DAL/Repositories/GoodsRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using DAL.Models;
+using SalesDataModel;
+
+namespace DAL.Repositories
+{
+    public class GoodsRepository : AbstractRepository, IRepository<GoodsDTO>
+    {
+       private Goods ToEntity(GoodsDTO source)
+        {
+            return new Goods() { Id = source.Id, Name = source.Name };
+        }
+
+        private GoodsDTO ToObject(Goods source)
+        {
+            return new GoodsDTO() { Id = source.Id, Name = source.Name };
+        }
+
+        public void Add(GoodsDTO item)
+        {
+            var e = this.ToEntity(item);
+            context.GoodsSet.Add(e);
+        }
+
+        public void Remove(GoodsDTO item)
+        {
+            var e = this.ToEntity(item);
+            context.GoodsSet.Remove(e);
+        }
+
+        public void Update(GoodsDTO item)
+        {
+            var itm = context.GoodsSet.FirstOrDefault(x => x.Id == item.Id);
+            if (itm != null)
+            {
+                itm.Name = item.Name;
+                context.Entry(itm).State = EntityState.Modified;
+            }
+        }
+
+
+        public IEnumerable<GoodsDTO> Items
+        {
+            get
+            {
+                foreach (var g in this.context.GoodsSet)
+                {
+                    yield return this.ToObject(g);
+                }
+            }
+        }
+
+        public void SaveSales()
+        {
+            this.context.SaveChanges();
+        }
+
+        public int Count
+        {
+            get { return this.context.GoodsSet.Count(); }
+        }
+    }
+}
---
+++ DbAutoMVC/DAL/Repositories/ManagerRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using DAL.Models;
+using SalesDataModel;
+
+namespace DAL.Repositories
+{
+    public class ManagerRepository : AbstractRepository, IRepository<ManagerDTO>
+    {
+        private Manager ToEntity(ManagerDTO source)
+        {
+            return new Manager() { Id = source.Id, FirstName = source.FirstName, 
+                                  SecondName = source.SecondName };
+        }
+
+        private ManagerDTO ToObject(Manager source)
+        {
+            return new ManagerDTO() { Id = source.Id, FirstName = source.FirstName, 
+                                      SecondName = source.SecondName };
+        }
+
+        public void Add(ManagerDTO item)
+        {
+            var e = this.ToEntity(item);
+            context.ManagerSet.Add(e);
+        }
+
+        public void Remove(ManagerDTO item)
+        {
+            var e = this.ToEntity(item);
+            context.ManagerSet.Remove(e);
+        }
+
+        public void Update(ManagerDTO item)
+        {
+            var itm = context.ManagerSet.FirstOrDefault(x => x.Id == item.Id);
+            if (itm != null)
+            {
+                context.Entry(itm).State = EntityState.Modified;
+            }
+        }
+
+
+
+       public IEnumerable<ManagerDTO> Items
+        {
+            get
+            {
+                foreach (var m in this.context.ManagerSet)
+                {
+                    yield return this.ToObject(m);
+                }
+            }
+        }
+
+        public void SaveSales()
+        {
+            context.SaveChanges();
+        }
+
+        public int Count
+        {
+            get { return this.context.ManagerSet.Count(); }
+        }
+    }
+}
