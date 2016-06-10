d1cf51ce55dcefbe37928baab1e550daba0376b7
KorolkoIgor <korolko_igor@mail.ru>
2016-06-10 23:39:35 +0300

    Signed-off-by: KorolkoIgor <korolko_igor@mail.ru>

---
+++ DbAutoMVC/DAL/Models/SalesDTO.cs
@@ -0,0 +1,32 @@
+﻿using SalesDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class SalesDTO
+    {
+        private readonly  DataModelContainer1 _dbServiceEntities = new  DataModelContainer1();
+        
+        public int Id { get; set; }
+        public System.DateTime Date { get; set; }
+        public ClientDTO Client { get; set; }
+        public GoodsDTO Goods { get; set; }
+        public ManagerDTO Manager { get; set; }
+        public double Cost { get; set; }
+       
+        
+        public SalesDTO(int id, DateTime date, ClientDTO clientId, GoodsDTO goodsId, ManagerDTO managerId, double cost)
+        {
+            Id = id;
+            Date = date;
+            Client = clientId;
+            Goods = goodsId;
+            Manager = managerId;
+            Cost = cost;
+        }
+
+     }
+}
---
+++ DbAutoMVC/DAL/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+       // void Update(T item);
+       IEnumerable<T> Items { get; }
+        void SaveSales();
+        int Count { get; }
+    }
+}
